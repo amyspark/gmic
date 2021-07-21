e07ce1e9f13c79cea7ac7be17f75b1227893c639
L. E. Segovia <amy@amyspark.me>
2021-07-21 01:07:38 +0000

    Enable respecting host application language

--- gmic-qt/src/DialogSettings.cpp
+++ gmic-qt/src/DialogSettings.cpp
@@ -122,6 +122,13 @@ DialogSettings::DialogSettings(QWidget * parent) : QDialog(parent), ui(new Ui::D
   connect(ui->cbNotifyFailedUpdate, SIGNAL(toggled(bool)), this, SLOT(onNotifyStartupUpdateFailedToggle(bool)));
 #endif
 
+#ifdef _GMIC_QT_DISABLE_TRANSLATION_
+  {
+    // Pierce the veil and disable the combobox of the language selector
+    auto *combobox = ui->languageSelector->findChild<QComboBox*>("comboBox");
+    if (combobox) combobox->setEnabled(false);
+  }
+#endif
   ui->languageSelector->selectLanguage(Settings::languageCode());
   ui->languageSelector->enableFilterTranslation(Settings::filterTranslationEnabled());
 
